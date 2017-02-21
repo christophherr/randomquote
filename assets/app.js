8fbd3443c74bdfb72808ee88f52bc917c893ee4d
christophherr <info@christophherr.com>
2017-02-21 15:53:02 -0500

    Changes for test suite

--- assets/app.js
+++ assets/app.js
@@ -1,23 +1,21 @@
-$(document).ready(function () {
-  $(".get-new-quote-button").on("click", function(getQuote) {
-    getQuote.preventDefault(),
+$(document).ready( function() {
+  $('.get-new-quote-button').on('click', function(getQuote) {
+    getQuote.preventDefault();
     $.ajax({
-      url: "http://quotesondesign.com/wp-json/posts?filter[orderby]=rand&filter[posts_per_page]=1",
+      url: 'https://quotesondesign.com/wp-json/posts?filter[orderby]=rand&filter[posts_per_page]=1',
       success: function(getQuote) {
                 var content = getQuote.shift();
-                $(".quote-author").text(content.title),
-                $(".quote").html(content.content),
-                "undefined" != typeof content.custom_meta && "undefined" != typeof content.custom_meta.Source ? $(".quote-source").html("Source: " + content.custom_meta.Source) : $(".quote-source").text("");
+                $('.quote-author').html(content.title);
+                $('.quote').html(content.content);
+                'undefined' !== typeof content.custom_meta && 'undefined' !== typeof content.custom_meta.Source ? $('.quote-source').html('Source: ' + content.custom_meta.Source) : $('.quote-source').text('');
 
-                var twitter = "http://twitter.com/home?status=",
-                twitterMessage = content.content.replace(/(<([^>]+)>)/gi, ""),
+                var twitter = 'http://twitter.com/intent/tweet/?text=',
+                twitterMessage = content.content.replace(/(<([^>]+)>)/gi, ''),
                 twitterMessageShortened = twitterMessage.substring(0, 108);
 
-                twitter += twitterMessageShortened + "...",
-                twitter += " " + content.link,
-                $("#twitter").attr("href", twitter),
-                window.history.replaceState({}, "", content.link),
-                window.document.title = "Quotes on Design"
+                twitter += twitterMessageShortened + '...';
+                twitter += ' ' + content.link;
+                $('#tweet-quote').attr('href', twitter);
               },
       cache: !1
     });
