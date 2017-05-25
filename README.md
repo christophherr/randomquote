9b1fff3c2e4dd30c63f1e667090427b1bbb3aaff
christophherr <info@christophherr.com>
2017-05-25 11:20:32 -0400

    Removes jQuery, adds Axios

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
-# randomquote
-Random Quote Generator for Free Code Camp's Front End Certification
+# Random Quote Generator
+Inspired by freeCodeCamp's Front End Certification
 
 Uses the wp-json api from http://quotesondesign.com.
 
-Functional CodePen: http://codepen.io/c-herr/full/ONGwBN
+CodePen: http://codepen.io/c-herr/full/ONGwBN
--- assets/app.js
+++ assets/app.js
@@ -1,23 +1,37 @@
-$(document).ready( function() {
-  $('.get-new-quote-button').on('click', function(getQuote) {
-    getQuote.preventDefault();
-    $.ajax({
-      url: 'https://quotesondesign.com/wp-json/posts?filter[orderby]=rand&filter[posts_per_page]=1',
-      success: function(getQuote) {
-                var content = getQuote.shift();
-                $('.quote-author').html(content.title);
-                $('.quote').html(content.content);
-                'undefined' !== typeof content.custom_meta && 'undefined' !== typeof content.custom_meta.Source ? $('.quote-source').html('Source: ' + content.custom_meta.Source) : $('.quote-source').text('');
+/* global axios */
 
-                var twitter = 'http://twitter.com/intent/tweet/?text=',
-                twitterMessage = content.content.replace(/(<([^>]+)>)/gi, ''),
-                twitterMessageShortened = twitterMessage.substring(0, 108);
+var apiUrl =
+    'https://quotesondesign.com/wp-json/posts?filter[orderby]=rand&filter[posts_per_page]=1?v=',
+    newQuote = document.getElementById('new-quote'),
+    quoteAuthor = document.getElementById('author'),
+    quoteText = document.getElementById('text'),
+    quoteSource = document.getElementById('source'),
+    tweetQuote = document.getElementById('tweet-quote'),
+    content,
+    tweet,
+    twitterUrl = 'http://twitter.com/intent/tweet/?text=',
+    twitterMessage,
+    twitterMessageShortened;
 
-                twitter += twitterMessageShortened + '...';
-                twitter += ' ' + content.link;
-                $('#tweet-quote').attr('href', twitter);
-              },
-      cache: !1
+newQuote.addEventListener('click', function(event) {
+    console.log(event);
+    axios.get(apiUrl + Date.now()).then(function(response) {
+        content = response.data[0];
+
+        quoteAuthor.innerHTML = content.title;
+        quoteText.innerHTML = content.content;
+        typeof content.custom_meta !== 'undefined' &&
+            typeof content.custom_meta.Source !== 'undefined'
+            ? (quoteSource.innerHTML = 'Source: ' + content.custom_meta.Source)
+            : (quoteSource.innerHTML = null);
+
+        twitterMessage = content.content
+            .trim()
+            .replace(/(<([^>]+)>)/gi, '')
+            .replace(/'/gi, '´');
+        twitterMessageShortened = twitterMessage.substring(0, 108);
+
+        tweet = twitterUrl + twitterMessageShortened + '... ' + content.link;
+        tweetQuote.setAttribute('href', tweet);
     });
-  });
 });
