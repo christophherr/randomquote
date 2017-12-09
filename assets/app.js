d5a6aedaac5b71ad88d90eab2f14e0d0345cdd3a
Christoph Herr <info@christophherr.com>
2017-12-08 20:18:59 -0500

    Updated code style

--- assets/app.js
+++ assets/app.js
@@ -1,37 +1,40 @@
 /* global axios */
 
-var apiUrl =
-    'https://quotesondesign.com/wp-json/posts?filter[orderby]=rand&filter[posts_per_page]=1?v=',
-    newQuote = document.getElementById('new-quote'),
-    quoteAuthor = document.getElementById('author'),
-    quoteText = document.getElementById('text'),
-    quoteSource = document.getElementById('source'),
-    tweetQuote = document.getElementById('tweet-quote'),
-    content,
-    tweet,
-    twitterUrl = 'http://twitter.com/intent/tweet/?text=',
-    twitterMessage,
-    twitterMessageShortened;
+const apiUrl =
+  'https://quotesondesign.com/wp-json/posts?filter[orderby]=rand&filter[posts_per_page]=1?v=';
+const newQuote = document.getElementById('new-quote');
+const quoteAuthor = document.getElementById('author');
+const quoteText = document.getElementById('text');
+const quoteSource = document.getElementById('source');
+const tweetQuote = document.getElementById('tweet-quote');
+const twitterUrl = 'http://twitter.com/intent/tweet/?text=';
+let content;
+let tweet;
+let twitterMessage;
+let twitterMessageShortened;
 
-newQuote.addEventListener('click', function(event) {
-    console.log(event);
-    axios.get(apiUrl + Date.now()).then(function(response) {
-        content = response.data[0];
+newQuote.addEventListener('click', () => {
+  axios.get(apiUrl + Date.now()).then((response) => {
+    content = response.data[0];
 
-        quoteAuthor.innerHTML = content.title;
-        quoteText.innerHTML = content.content;
-        typeof content.custom_meta !== 'undefined' &&
-            typeof content.custom_meta.Source !== 'undefined'
-            ? (quoteSource.innerHTML = 'Source: ' + content.custom_meta.Source)
-            : (quoteSource.innerHTML = null);
+    quoteAuthor.innerHTML = content.title;
+    quoteText.innerHTML = content.content;
+    if (
+      typeof content.custom_meta !== 'undefined' &&
+      typeof content.custom_meta.Source !== 'undefined'
+    ) {
+      quoteSource.innerHTML = `Source: ${content.custom_meta.Source}`;
+    } else {
+      quoteSource.innerHTML = null;
+    }
 
-        twitterMessage = content.content
-            .trim()
-            .replace(/(<([^>]+)>)/gi, '')
-            .replace(/'/gi, '´');
-        twitterMessageShortened = twitterMessage.substring(0, 108);
+    twitterMessage = content.content
+      .trim()
+      .replace(/(<([^>]+)>)/gi, '')
+      .replace(/'/gi, '´');
+    twitterMessageShortened = twitterMessage.substring(0, 108);
 
-        tweet = twitterUrl + twitterMessageShortened + '... ' + content.link;
-        tweetQuote.setAttribute('href', tweet);
-    });
+    tweet = `${twitterUrl + twitterMessageShortened}... ${content.link}`;
+    tweetQuote.setAttribute('href', tweet);
+  });
 });
